06d9e69d2a57c00f80e15fbe774491e483552769
Vinmathi13 <vinmathiravichandran@gmail.com>
2026-05-16 22:28:24 -0500

    Add files via upload
    week 1

---
+++ week1 day1/gradecalc.js
@@ -0,0 +1,37 @@
+function getGrade(score) {
+    let grade;
+
+    switch (true) {
+        case (score >= 90 && score <= 100):
+            grade = "A";
+            break;
+
+        case (score >= 80 && score < 90):
+            grade = "B";
+            break;
+
+        case (score >= 70 && score < 80):
+            grade = "C";
+            break;
+
+        case (score >= 60 && score < 70):
+            grade = "D";
+            break;
+
+        case (score >= 0 && score < 60):
+            grade = "F";
+            break;
+
+        default:
+            grade = "Invalid Score";
+    }
+
+    return grade;
+}
+
+// initialize score 
+let studentScore = 65;
+
+// Call function and print result
+let result = getGrade(studentScore);
+console.log("Student Grade:", result);
